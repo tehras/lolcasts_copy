8b97890490ab2b88b0fe9f2a465a2b9602d0ea75
Tehras <tkoshkin@gmail.com>
2013-11-30 00:25:10 -0500

    new layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -254,5 +254,30 @@ $(total_id).html(votevalue_abs)
 });
 }
 
+$(function () {
+
+    //Custom Checkbox For Light Theme
+    $("input").iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        increaseArea: '20%'
+    });
+
+
+    //Custom Checkbox For Dark Theme
+    $(".dark input").iCheck({
+        checkboxClass: 'icheckbox_polaris',
+        increaseArea: '20%'
+    });
+
+
+    //TextBox Focus Event
+    $(".form-control").focus(function () {
+        $(this).closest(".textbox-wrap").addClass("focused");
+    }).blur(function () {
+            $(this).closest(".textbox-wrap").removeClass("focused");
+        });
+
+});
+
 
 
