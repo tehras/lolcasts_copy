6f8403fb2d31390aaea3ade092f06d5726b8fd8c
Tehras <tkoshkin@gmail.com>
2013-12-08 22:19:42 -0500

    Created Better Error Checking New Cast

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,7 +41,6 @@ ajax_bo = function(best_of) {
         $("#link"+ n.toString()).removeAttr("Style");
         n++;
     }
-    $("#submit").removeAttr("Style");
 }
 
 ajax_match = function(match_type){
@@ -81,6 +80,14 @@ ajax_match = function(match_type){
         $("#"+error+"_field").keyup( function(){
             if( $("#"+error+"_field").val().length < range1 || $('#red_team_field').val().length >= range2 )
             {
+                $("#best_of").attr("Style","display: none");
+                $("#link1").attr("Style","display: none");
+                $("#link2").attr("Style","display: none");
+                $("#link3").attr("Style","display: none");
+                $("#link4").attr("Style","display: none");
+                $("#link5").attr("Style","display: none");
+                $("#link6").attr("Style","display: none");
+                $("#link7").attr("Style","display: none");
                 $("#"+error+"_error").removeAttr("Style");
                 $("#"+error+"_error").attr("Style","color:red");
                 if (eval(error.concat("_flag"))== red_team_flag)
@@ -302,6 +309,115 @@ $(function () {
         }
     });
     }
+function formFiller(){
+youtube_checker('link1');
+youtube_checker('link2');
+youtube_checker('link3');
+youtube_checker('link4');
+youtube_checker('link5');
+youtube_checker('link6');
+youtube_checker('link7');
+var link1_flag;
+var link2_flag;
+var link3_flag;
+var link4_flag;
+var link5_flag;
+var link6_flag;
+var link7_flag;
+
+function youtube_checker(link){
+    $('#'+link+'_field').keyup(function()
+    {
+        var return_value = youtube_parser($('#'+link+'_field').val());
+        if (return_value == null){
+            $('#'+link+'_error').removeAttr('Style');
+            $('#'+link+'_error').attr('Style','color:red');
+            $('#'+link+'_error').text('*Not Correct! Ex(http://www.youtube.com/watch?v=XXXXXXXXXXX)')
+            $('#submit').attr('Style','display:none');
+        }
+        else {
+            $('#'+link+'_error').attr('Style','color:green')
+            $('#'+link+'_error').text('Success! YouTube Video with a link of '+return_value+' will be displayed!')
+            var best_of = $("#best_of_hidden").val();
+            if (eval(link.concat("_flag"))== link1_flag)
+            {
+                link1_flag = true;
+            }
+            else if (eval(link.concat("_flag"))== link2_flag)
+            {
+                link2_flag = true;
+            }
+            else if (eval(link.concat("_flag"))== link3_flag)
+            {
+                link3_flag = true;
+            }
+            else if (eval(link.concat("_flag"))== link4_flag)
+            {
+                link4_flag = true;
+            }
+            else if (eval(link.concat("_flag"))== link5_flag)
+            {
+                link5_flag = true;
+            }
+            else if (eval(link.concat("_flag"))== link6_flag)
+            {
+                link6_flag = true;
+            }
+            else if (eval(link.concat("_flag"))== link7_flag)
+            {
+                link7_flag = true;
+            }
+            else
+            {
+
+            }
+
+            if (best_of == 1)
+            {
+                if (link1_flag == true)
+                {
+                    $('#submit').removeAttr('Style');
+                }
+            }
+            else if (best_of == 3)
+            {
+                if (link1_flag == true && link2_flag == true && link3_flag == true)
+                {
+                    $('#submit').removeAttr('Style');
+                }
+            }
+            else if (best_of == 5)
+            {
+                if (link1_flag == true && link2_flag == true && link3_flag == true && link4_flag == true && link5_flag == true)
+                {
+                    $('#submit').removeAttr('Style');
+                }
+            }
+            else if (best_of == 7)
+            {
+                if (link1_flag == true && link2_flag == true && link3_flag == true && link4_flag == true && link5_flag == true && link6_flag == true && link7_flag == true)
+                {
+                    $('#submit').removeAttr('Style');
+                }
+            }
+            else {
+                $('#submit').attr('Style','display:none');
+            }
+        }
+    })
+}
+
+function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match&&match[7].length==11){
+        return match[7];
+    }else{
+        return null;
+    }
+}
+}
+
 
 
 
