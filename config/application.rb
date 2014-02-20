06b742c9fdcc50df8e4a26e1582b4917fdbd7a0c
Tehras <tkoshkin@gmail.com>
2014-02-19 21:40:52 -0500

    Rolify

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
 
 module Lolcasts
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
