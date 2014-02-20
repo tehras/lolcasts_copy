96c22dd52b51dea387a7b10ccc8f386a83b51710
Tehras <tkoshkin@gmail.com>
2014-02-19 21:33:50 -0500

    Rolify

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+
 
 # Load application ENV vars and merge with existing ENV vars. Loaded here so can use values in initializers.
 ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
